963d3d4f8da78d7355c042cf2c5ec214fa0953d1
bhaskarpande <mfsi.bhaskarp@gmail.com>
2016-11-12 18:01:38 +0530

    Modified to include ip along with port for listening Source: http://stackoverflow.com/questions/28703872/openshift-always-gives-503-error-for-node-js-app

--- app.js
+++ app.js
@@ -13,9 +13,10 @@ var express = require('express')
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('ip', process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0');
+//app.set('views', __dirname + '/views');
+//app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
@@ -30,8 +31,8 @@ if ('development' == app.get('env')) {
 
 //app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
+  console.log('Express server listening on port ' + app.get('port')+","+app.get('ip'));
 });
 
 
